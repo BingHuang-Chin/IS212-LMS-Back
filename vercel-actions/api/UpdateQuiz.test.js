4d3aa45a2a3c9371940a47a477f6aa621f3b250f
Bing Huang <bing2609@gmail.com>
2021-10-04 01:57:35 +0800

    vercel-actions: UpdateQuizTest: initial test script

---
+++ vercel-actions/api/UpdateQuiz.test.js
@@ -0,0 +1,98 @@
+const { handleProcess, retrieveBodyData } = require("./UpdateQuiz")
+
+test("Empty input", () => {
+  const mockQuizChanges = {}
+  const retrievedData = retrieveBodyData(mockQuizChanges)
+  expect(retrievedData).toEqual({ error: { status: 400, message: "Invalid input provided." } })
+})
+
+test("Populated input", () => {
+  const mockQuizChanges = {
+    input: {
+      "object": {
+        "title": "Quiz 3",
+        "section_id": 1,
+        "time_limit": 1,
+        "questions": {
+          "data": [
+            {
+              "title": "New question 2",
+              "question_type_id": 1,
+              "question_options": {
+                "data": [
+                  {
+                    "is_answer": false,
+                    "title": "some option 3",
+                    "id": 6
+                  },
+                  {
+                    "is_answer": true,
+                    "title": "some option 6",
+                    "id": 16
+                  },
+                  {
+                    "is_answer": false,
+                    "title": "some option 7",
+                    "id": 17
+                  },
+                  {
+                    "is_answer": false,
+                    "title": "some option 8",
+                    "id": 21
+                  }
+                ]
+              },
+              "id": 5
+            },
+            {
+              "title": "Question 3",
+              "question_type_id": 1,
+              "question_options": {
+                "data": [
+                  {
+                    "is_answer": false,
+                    "title": "Goodnight",
+                    "id": 20
+                  },
+                  {
+                    "is_answer": true,
+                    "title": "Hello world",
+                    "id": 18
+                  },
+                  {
+                    "is_answer": false,
+                    "title": "Goodbye",
+                    "id": 19
+                  }
+                ]
+              },
+              "id": 10
+            },
+            {
+              "title": "question 1",
+              "question_type_id": 2,
+              "question_options": {
+                "data": [
+                  {
+                    "is_answer": true,
+                    "title": "False",
+                    "id": 3
+                  },
+                  {
+                    "is_answer": false,
+                    "title": "True",
+                    "id": 15
+                  }
+                ]
+              },
+              "id": 4
+            }
+          ]
+        },
+        "id": 8
+      }
+    }
+  }
+  const retrievedData = retrieveBodyData(mockQuizChanges)
+  expect(retrievedData).toEqual(mockQuizChanges.input.object)
+})
