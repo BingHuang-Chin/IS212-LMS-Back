877dbdfa57e45de739940783f085c88a9f295e38
Vera Chua <vera.chua.2019@scis.smu.edu.sg>
2021-10-26 22:15:58 +0800

    TDD for check enrolment end date

--- vercel-actions/api/self_enrol.js
+++ vercel-actions/api/self_enrol.js
@@ -1,4 +1,6 @@
 //Main function which will be triggered when the browser requests for http://localhost:3001/api/self_enrol
+const fetch = require("node-fetch")
+
 const vercelFn = async (request, response) => {
     return response.json({ hello : "world" })
 }
@@ -8,7 +10,7 @@ const selfEnrol = (learnerId, courseId) => {
     return true
 }
 
-const checkEnrolmentEndDate = (enrolment_end_date) => {
+function checkEnrolmentEndDate (enrolment_end_date, role) {
     //TODO: Processing
     var checkQuery = `query {
             course(where: {enrolment_end_date: {_gte: "${enrolment_end_date}"}}) {
@@ -16,9 +18,39 @@ const checkEnrolmentEndDate = (enrolment_end_date) => {
             id
             }
         }`  
+    // console.log(checkQuery)
+    return new Promise(resolve => {
+        fetch("http://localhost:8080/v1/graphql", {
+            method: "POST",
+            headers: {
+            "Content-Type": "application/json",
+            "x-hasura-admin-secret": "myadminsecretkey"
+            // "x-hasura-role": role
+            },
+            body: JSON.stringify({
+            query: checkQuery
+            })
+        })
+            .then(response => response.json())
+            .then(({ errors, data }) => {
+                // console.log(errors, data)
+            if (errors)
+                return resolve({ error: { status: 500, message: "Internal server error." } })
+    
+            // if (!data.quiz_by_pk)
+            //   resolve({ error: { status: 404, message: "Course not found." } })
+    
+            return resolve({ data: { ...data, status: 200 } })
+            })
+            .catch(() => {
+            // console.log("errors")
+            return resolve({ error: { status: 500, message: "Internal server error." } })
+            })
+        })
 }
 
 module.exports = {
     selfEnrol,
+    checkEnrolmentEndDate,
     default : vercelFn
 }
--- vercel-actions/api/self_enrol.test.js
+++ vercel-actions/api/self_enrol.test.js
@@ -1,6 +1,30 @@
-const { selfEnrol } = require("./self_enrol")
+const { selfEnrol, checkEnrolmentEndDate } = require("./self_enrol")
+const fetch = require("node-fetch")
+jest.mock("node-fetch", () => jest.fn())
 
 test("Self enrol course", () => {
     const enrolled = selfEnrol(1,1)
     expect(enrolled).toBe(true)
+})
+
+
+test("Check Enrolment End Date", async () => {
+    const expectedResponse = {
+        "data": {
+          "course": [
+            {
+              "enrolment_end_date": "2016-01-25T00:00:00+00:00",
+              "id": 10
+            }
+          ]
+        }
+      }
+
+    fetch.mockImplementation(() => Promise.resolve({ json: () => expectedResponse }))
+  
+    const { data } = await checkEnrolmentEndDate("2021-10-12", "learner")
+    const { status, ...remainingData } = data
+  
+    expect(status).toBe(200)
+    expect(remainingData).toEqual(expectedResponse.data)
 })
