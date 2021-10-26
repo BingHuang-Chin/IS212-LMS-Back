6813d7d4e7dc66740ac2c911c94618f34b7268eb
Vera Chua <vera.chua.2019@scis.smu.edu.sg>
2021-10-26 21:45:57 +0800

    26/10

--- vercel-actions/api/self_enrol.test.js
+++ vercel-actions/api/self_enrol.test.js
@@ -2,5 +2,5 @@ const { selfEnrol } = require("./self_enrol")
 
 test("Self enrol course", () => {
     const enrolled = selfEnrol(1,1)
-    expect(enrolled).toBe(false)
+    expect(enrolled).toBe(true)
 })
