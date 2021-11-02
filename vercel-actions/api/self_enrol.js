60bfc0eabef99c7a74784c5c5f01ebee2f232c88
Vera Chua <vera.chua.2019@scis.smu.edu.sg>
2021-11-02 19:23:22 +0800

    learners withdraw from class halfway done

--- vercel-actions/api/self_enrol.js
+++ vercel-actions/api/self_enrol.js
@@ -90,11 +90,50 @@ function checkClassSize (class_size, role) {
             return resolve({ error: { status: 500, message: "Internal server error." } })
             })
         })
+
+function withdrawClass (class_size, role) {
+    //TODO: Processing
+    var checkQuery = `query {
+            course(where: {class_size: {_gte: "${class_size}"}}) {
+            class_size
+            id
+            }
+        }`  
+    // console.log(checkQuery)
+    return new Promise(resolve => {
+        fetch("http://localhost:8080/v1/graphql", {
+            method: "POST",
+            headers: {
+            "Content-Type": "application/json",
+            "x-hasura-admin-secret": "myadminsecretkey",
+            "x-hasura-role": role
+            },
+            body: JSON.stringify({
+            query: checkQuery
+            })
+        })
+            .then(response => response.json())
+            .then(({ errors, data }) => {
+                // console.log(errors, data)
+            if (errors)
+                return resolve({ error: { status: 500, message: "Internal server error." } })
+    
+            // if (!data.quiz_by_pk)
+            //   resolve({ error: { status: 404, message: "Course not found." } })
+    
+            return resolve({ data: { ...data, status: 200 } })
+            })
+            .catch(() => {
+            // console.log("errors")
+            return resolve({ error: { status: 500, message: "Internal server error." } })
+            })
+        })
 }
 
 module.exports = {
     // selfEnrol,
     checkEnrolmentEndDate,
     checkClassSize,
+    withdrawClass,
     default : vercelFn
 }
