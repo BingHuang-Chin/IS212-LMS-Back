7f5e05a56d21c5714f37cb05ab5efb0749415ba6
Bing Huang <bing2609@gmail.com>
2021-11-11 01:13:54 +0800

    migrations: default: fixed compeleted_course foreign key migration
    Signed-off-by: Bing Huang <bing2609@gmail.com>

--- hasura-migrations/migrations/default/1635350870850_delete_fk_public_completed_course_completed_course_learner_id_fkey2/down.sql
+++ hasura-migrations/migrations/default/1635350870850_delete_fk_public_completed_course_completed_course_learner_id_fkey2/down.sql
@@ -1,5 +1,5 @@
 alter table "public"."completed_course"
-  add constraint "completed_course_learner_id_fkey2"
+  add constraint "completed_course_learner_id_fkey"
   foreign key ("learner_id")
   references "public"."learner"
   ("id") on update cascade on delete cascade;
--- hasura-migrations/migrations/default/1635350870850_delete_fk_public_completed_course_completed_course_learner_id_fkey2/up.sql
+++ hasura-migrations/migrations/default/1635350870850_delete_fk_public_completed_course_completed_course_learner_id_fkey2/up.sql
@@ -1,1 +1,1 @@
-alter table "public"."completed_course" drop constraint "completed_course_learner_id_fkey2";
+alter table "public"."completed_course" drop constraint "completed_course_learner_id_fkey";
